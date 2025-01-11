fcb42e87f29518938569450147a23351c50bd9df
Eric <epstringskip@gmail.com>
2025-01-10 18:08:50 -0800

    added folder for Header component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Header from './components/Header.tsx'
+import Header from './components/Header/Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
