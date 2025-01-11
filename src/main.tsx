92736b1a0a6fe1709ef72ca623baf7d78054481a
Eric <epstringskip@gmail.com>
2025-01-10 17:59:47 -0800

    setup components folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './Header.tsx'
+import Header from './components/Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
